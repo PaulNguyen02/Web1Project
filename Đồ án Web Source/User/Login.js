f32f2f62116146cc59c11d31c73decb9746c498d
Phúc Nguyễn <105428358+PaulNguyen02@users.noreply.github.com>
2022-10-26 23:14:21 +0700

    Delete Login.js

--- Đồ án Web Source/User/Login.js
+++
@@ -1,8 +0,0 @@
-function Add()
-{
-    var name=document.getElementById("user").value;
-    var psw=document.getElementById("password").value;
-    var user={username: name, password: psw};
-    var text = JSON.stringify(user);
-    alert("Tài khoản đã lưu "+text);
-}
